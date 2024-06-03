8279dfc3dcb37f2f99d6d7e881b33d76d38b2e04
evikzub <evikzub@gmail.com>
2024-06-04 00:12:28 +0300

    session adjustment

--- conftest.py
+++ conftest.py
@@ -45,39 +45,22 @@ def admin_login(browser: Browser):
     if not admin_session:
         # get cookie from the file
         session = retrieve_cookie()
-        if session:
-            with browser.new_context(extra_http_headers=get_header(session)) as context:
-                page = context.new_page()
-                # page.goto(ADMIN_URL, wait_until='networkidle')
-                page.goto(ADMIN_URL)
-
-                # Check if session is valid
-                if page.url == ADMIN_URL:
-                    set_admin_session(session)
-                else:
-                    m = Login(page, is_admin=True)
-                    m.user_login()
-                    for cookie in context.cookies():
-                        if "residential_energy_partners_session" == cookie["name"]:
-                            admin_session = cookie["value"]
-                            set_admin_session(session)
-                            store_cookie(admin_session)
-                            break
-
-        # generate new session if a value from the file is not valid
-        # print(">>> Generate new admin session <<<")
-        # with browser.new_context() as context:
-        #     page = context.new_page()
-        #     m = Login(page, is_admin=True)
-        #     m.user_login()
-        #     for cookie in context.cookies():
-        #         if "residential_energy_partners_session" == cookie["name"]:
-        #             admin_session = cookie["value"]
-        #             os.environ["ADMIN_SESSION"] = admin_session
-        #             store_cookie(admin_session)
-        #             break
-
-
-# @pytest.fixture(scope="function")
-# def admin_session() -> str:
-#     return get_admin_session()
+        with browser.new_context(extra_http_headers=get_header(session)) as context:
+            page = context.new_page()
+            # page.goto(ADMIN_URL, wait_until='networkidle')
+            page.goto(ADMIN_URL)
+            # print(f" {page.url} : {ADMIN_URL}")
+            # Check if session is valid
+            if len(session) > 0 and page.url == ADMIN_URL:
+                print(">>> Set admin session <<<")
+                set_admin_session(session)
+            else:
+                print(">>> Open admin page <<<")
+                m = Login(page, is_admin=True)
+                m.user_login()
+                for cookie in context.cookies():
+                    if "residential_energy_partners_session" == cookie["name"]:
+                        admin_session = cookie["value"]
+                        set_admin_session(admin_session)
+                        store_cookie(admin_session)
+                        break
